1756897c3b180447370dde6e0579f38977b5e3d7
KlausVigo <klaus.schliep@gmail.com>
2015-02-10 16:49:36 -0500

    Starting putting it together

--- server.R
+++ server.R
@@ -6,18 +6,88 @@
 #
 
 library(shiny)
+library(phytools)
+
+readTrees <- function(file, format="phylip"){
+  if(format=="phylip")tree = read.tree(file)
+  else tree = read.nexus(file)
+#  if(class(tree)=="phylo")tree = list(tree)
+  tree
+}
 
 shinyServer(function(input, output) {
 
+  treeInput <- reactive({
+    inFile <- input$fileTree
+    if (is.null(inFile))
+      return(NULL)
+    tmp = strsplit(tolower(input$fileTree$name), "[.]")[[1]]
+    tmp = tmp[length(tmp)]
+    if(is.na(pmatch("nex", tmp)))xx$format="phylip"
+    else xx$format="nexus"
+    readTrees(input$fileTree$datapath, xx$format)
+  })
+
+  dataInput <- reactive({
+    inFile <- input$fileCSV
+    if (is.null(inFile))
+      return(NULL)
+    read.csv(inFile$datapath, row.names=1)
+  })
+  
+  D <- reactiveValues()
+  D$colnames=NULL
+  observe({
+    if(input$refreshButton){ 
+      D$data <- dataInput()
+      D$colnames <- colnames(D$data)     
+      D$disCont <- sapply(D$data, "class")
+    }                      
+  })
+
+  
+#  X <- reactiveValues()
+#  X$tree <- treeInput()
+  
+    xx <- reactiveValues()
+    xx$format <- "phylip"
+    xx$type <- "phylogram"
+    xx$font <- 1
+    xx$phyloOrClado <- TRUE
+    xx$varnames <- NULL
+    observe({
+        if (input$phylogram != 0) {
+            xx$type <- "phylogram"
+            xx$phyloOrClado <- TRUE
+        }
+    })
+    observe({
+       if (input$fan != 0) {
+           xx$type <- "fan"
+           xx$phyloOrClado <- FALSE
+           }
+    })
+
+    output$chooseVariable <- renderUI({
+         if(length(D$colnames) > 0)
+         selectInput('chooseVariable', 'Choose Variable', D$colnames)
+    })
+
+
+    output$direction <- renderUI({
+       if(xx$type == 'phylogram' || xx$type == 'cladogram')
+           selectInput('direction', 'Direction',
+                c('rightwards', 'leftwards', 'upwards', 'downwards'))
+       })
+
+
   output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+#browser()
+    tree <- treeInput()
+    
+    plot(tree, type=xx$type, direction=input$direction)
+    
+    
   })
 
 })
--- ui.R
+++ ui.R
@@ -18,11 +18,33 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+      fileInput('fileTree', 'Import tree file'),
+      fileInput('fileCSV', 'Choose CSV File',
+          accept=c('text/csv', 
+             'text/comma-separated-values,text/plain', '.csv')),
+      actionButton("refreshButton", icon("refresh")),
+      
+      uiOutput('chooseVariable'),
+#      sliderInput("bins",
+#                  "Number of bins:",
+#                  min = 1,
+#                  max = 50,
+#                  value = 30),
+#Tree type
+      HTML("<div id=\"ptype\" class=\"btn-group\">
+           <button id=\"phylogram\"  type=\"button\" class=\"btn action-button\">\n  <img src=\"../phylogram.png\" width=\"40\" height=\"40\" alt=\"phylogram\"/>\n</button>\n
+           <button id=\"fan\" type=\"button\" class=\"btn action-button\">\n  <img src=\"../fan.png\" width=\"40\" height=\"40\" alt=\"fan\"/>\n</button>
+           </div>"),
+#Direction for phylogramm 
+      uiOutput('direction'),
+#Fonts for Labels
+      HTML("<div id=\"font\" class=\"btn-group\">
+<button id=\"font1\"  type=\"button\" class=\"btn action-button\">\n  <img src=\"../font1.svg\" width=\"40\" height=\"40\" alt=\"font1\"/>\n</button>\n
+<button id=\"font2\" type=\"button\" class=\"btn action-button\">\n  <img src=\"../font2.svg\" width=\"40\" height=\"40\" alt=\"font2\"/>\n</button>
+<button id=\"font3\" type=\"button\" class=\"btn action-button\">\n  <img src=\"../font3.svg\" width=\"40\" height=\"40\" alt=\"font3\"/>\n</button>
+<button id=\"font4\"type=\"button\" class=\"btn action-button\">\n  <img src=\"../font4.svg\" width=\"40\" height=\"40\" alt=\"font4\"/>\n</button>
+</div>")      
+      
     ),
 
     # Show a plot of the generated distribution
